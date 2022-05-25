4e65c92f5d61e6f49a3f5239f02be084cb4326f9
SiMiTaKu <gym.ts43@gmail.com>
2022-05-25 17:58:05 +0900

    JsValue内の定義変更(Category.Id・Todo.Id)

--- app/json/writes/WriteJsValueCategory.scala
+++ app/json/writes/WriteJsValueCategory.scala
@@ -1,17 +1,24 @@
 package json.writes
 
+import lib.model.Category
 import play.api.libs.json._
 import lib.persistence.default.CategoryRepository.EntityEmbeddedId
+import play.api.libs.functional.syntax.{toFunctionalBuilderOps, unlift}
 
 object WriteJsValueCategory {
   case class JsValueCategoryItem(
-                                id:    Long,
+                                id:    Category.Id,
                                 name:  String,
                                 slug:  String,
                                 color: Short,
                                 )
 
-  implicit val write: Writes[JsValueCategoryItem] = Json.writes[JsValueCategoryItem]
+  implicit val writes = (
+    (__ \ "id"    ).write[Long] ~
+    (__ \ "name"  ).write[String] ~
+    (__ \ "slug"  ).write[String] ~
+    (__ \ "color" ).write[Short]
+  )(unlift(JsValueCategoryItem.unapply))
 
   def list(categories: Seq[EntityEmbeddedId]): Seq[JsValueCategoryItem] = {
     categories.map{ category => single(category) }
@@ -19,7 +26,7 @@ object WriteJsValueCategory {
 
   def single(category: EntityEmbeddedId): JsValueCategoryItem = {
     JsValueCategoryItem(
-      id    = category.v.id match { case Some(id) => id case None => 0},
+      id    = category.v.id.getOrElse(Category.Id(0L)),
       name  = category.v.name,
       slug  = category.v.slug,
       color = category.v.color.code
--- app/json/writes/WriteJsValueTodo.scala
+++ app/json/writes/WriteJsValueTodo.scala
@@ -1,18 +1,26 @@
 package json.writes
 
+import lib.model.{Category, Todo}
 import play.api.libs.json._
 import lib.persistence.default.TodoRepository.EntityEmbeddedId
+import play.api.libs.functional.syntax.{toFunctionalBuilderOps, unlift}
 
 object WriteJsValueTodo {
   case class JsValueTodoItem(
-                                  id:          Long,
-                                  category_id: Long,
+                                  id:          Todo.Id,
+                                  category_id: Category.Id,
                                   title:       String,
                                   body:        String,
                                   state:       Short
                                 )
 
-  implicit val writes: Writes[JsValueTodoItem] = Json.writes[JsValueTodoItem]
+  implicit val writes = (
+    (__ \ "id"         ).write[Long] ~
+    (__ \ "category_id").write[Long] ~
+    (__ \ "title"      ).write[String] ~
+    (__ \ "body"       ).write[String] ~
+    (__ \ "state"      ).write[Short]
+  )(unlift(JsValueTodoItem.unapply))
 
   def list(todos: Seq[EntityEmbeddedId]): Seq[JsValueTodoItem] = {
     todos.map { todo => single(todo) }
@@ -20,7 +28,7 @@ object WriteJsValueTodo {
 
   def single(todo: EntityEmbeddedId): JsValueTodoItem = {
     JsValueTodoItem(
-      id          = todo.v.id match { case Some(id) => id case None => 0},
+      id          = todo.v.id.getOrElse(Todo.Id(0L)),
       category_id = todo.v.category_id,
       title       = todo.v.title,
       body        = todo.v.body,
